148620f8545187d26680e594a85f6ce391d441e4
Aniket Bhatnagar <aniket.bhatngar@gmail.com>
2013-12-14 04:28:54 +0530

    Fixed impl for bucket

--- sdk/platform.js
+++ sdk/platform.js
@@ -50,9 +50,9 @@ platform = (function(domain, appId) {
             this.receiveTilt(function(event){
                 callback({
                     "userId": event.userId,
-                    "tiltLR": event.tiltLR - event.tiltLR % bucket + bucket,
-                    "tiltFB": event.tiltFB- event.tiltFB % bucket + bucket,
-                    "dir": event.dir - event.dir % bucket + bucket,
+                    "tiltLR": event.tiltLR - event.tiltLR % bucket,
+                    "tiltFB": event.tiltFB- event.tiltFB % bucket,
+                    "dir": event.dir - event.dir % bucket,
                     "timestamp": event.timestamp
                 })
             })
